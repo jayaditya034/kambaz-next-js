04b9d6dd82c21ecc3f022eccafb1255888eab739
Jayaditya Peddisetti <jayadityapeddisetti@Jays-MacBook-Air.local>
2025-09-29 19:28:31 -0400

    Final Commit

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -3,10 +3,10 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
+      <input placeholder="username" className="wd-username" defaultValue="Jay"/><br/>
+      <input placeholder="password" type="password" className="wd-password" defaultValue="3399"/><br/>
       <input placeholder="verify password"
-             type="password" className="wd-password-verify" /><br/>
+             type="password" className="wd-password-verify" defaultValue="3399"/><br/>
       <Link  href="Profile" > Sign up </Link><br />
       <Link  href="Signin" > Sign in </Link>
     </div>
