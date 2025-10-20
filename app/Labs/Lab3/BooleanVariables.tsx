0a5246cdd1d66d3d67f80299ad33b7034a8b5657
Jayaditya Peddisetti <jay3399.cbit@gmail.com>
2025-10-20 01:27:13 -0400

    a3 JavaScript

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -1,5 +1,7 @@
 export default function BooleanVariables() {
-  const numberVariable = 123, floatingPointNumber = 234.345;
+  const numberVariable: number = 123;
+  const floatingPointNumber: number = 234.345;
+
   const true1 = true, false1 = false;
   const false2 = true1 && false1;
   const true2 = true1 || false1;
