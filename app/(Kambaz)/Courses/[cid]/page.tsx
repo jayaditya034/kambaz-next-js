bf024b2f39b59dc2cfdd62121f51b8e1ab70e76d
Jayaditya Peddisetti <jay3399.cbit@gmail.com>
2025-10-20 01:20:38 -0400

    a3 JavaScript

--- app/(Kambaz)/Courses/[cid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/page.tsx
@@ -1,13 +1,7 @@
-//export default function Courses() {
-  //return (
-    //<div id="wd-courses">
-      //<h2>Course 1234</h2>
-    //</div>
-//);}
 import { redirect } from "next/navigation";
 
-
-export default async function CoursesPage({ params, }: { params: Promise<{ cid: string }>; }) {
- const { cid } = await params;
- redirect(`/Courses/${cid}/Home`);
+export default function CoursesPage({ params }: { params: { cid: string } }) {
+  const { cid } = params;
+  redirect(`/Courses/${cid}/Home`);
+  return null; // ensure it’s a valid React component
 }
